b1f20baa35dc4a84cb3903ebca640d326431c669
Marie Liao <marie.liao@rosedigital.co>
2019-12-12 11:23:56 -0500

    Refactoring reg_real_ss_success file to use var and funct

--- nyl/reg_real_ss_success.py
+++ nyl/reg_real_ss_success.py
@@ -1,11 +1,13 @@
 # [Documentation - Setup] This section lists all dependencies
 # that are imported for this test file to work
-from selenium import webdriver
+from selenium import webdriver  #webdriver module provides all WebDriver implementations
 import warnings
-import unittest, time, re
-from selenium.webdriver.common.keys import Keys
-import var
-import funct
+import unittest, time, re       #unittest is the testing framework, provides module for organizing test cases
+from selenium.webdriver.common.keys import Keys     #Keys class provide keys in the keyboard like RETURN, F1, ALT, etc.
+from selenium.webdriver.common.by import By         #By class provides method for finding the page elements by NAME, ID, XPATH, etc.
+from selenium.webdriver.support.ui import Select    #Select class provides ability to select items in dropdown
+import var                                          #Custom class for NYL
+import funct                                        #Custom class for NYL
 
 # [Documentation - Summary] Tests user workflow of successful
 # registration with valid SSN4 and OTP pass
@@ -16,8 +18,12 @@ url = "https://sso-qa.nylservices.net/?clientId=4a0p01j46oms3j18l90lbtma0o&callb
 #url = "https://sso-stage.nylservices.net/?clientId=6pdeoajlh4ttgktolu3jir8gp6&callbackUri=https://google.com"
 testemail = "marie.liao+ssotest@rosedigital.co"
 
+# The test case class is inherited from unittest.TestCase.
+# Inheriting from TestCase class is the way to tell unittest module that this is a test case.
 class NYlotto(unittest.TestCase):
 
+# The setUp is part of initialization, this method will get called before every test function which you
+# are going to write in this test case class. Here you are creating the instance of Chrome WebDriver.
     def setUp(self):
         warnings.simplefilter("ignore", ResourceWarning)
         # self.driver = webdriver.Remote(
@@ -39,57 +45,71 @@ class NYlotto(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-#Test case
+# This is the test case method. The test case method should always start with the characters test.
+# The first line inside this method creates a local reference to the driver object created in setUp method.
     def test_reg(self):
         driver = self.driver
-        notepadfile = open('/Users/nylUser.txt', 'r')  #local file with user data
+# opens local file with user data
+        notepadfile = open('/Users/nylUser.txt', 'r')
+# variable for each line in the file
         entry_info = notepadfile.readlines()
+# The driver.get method will navigate to a page given by the URL.
+# WebDriver will wait until the page has fully loaded (that is, the “onload” event has fired)
+# before returning control to your test or script.
         driver.get(url)
+# Assertion that the title has Single Sign On in the title.
+        self.assertIn("Single Sign On", driver.title)
+# Instructions for webdriver to read and input user data via the info on the .txt doc.
+        funct.waitAndSend(driver, var.regV.fname, entry_info[0])
+        funct.waitAndSend(driver, var.regV.lname, entry_info[1])
+        funct.waitAndSend(driver, var.regV.housenum, entry_info[2])
+        funct.waitAndSend(driver, var.regV.street, entry_info[3])
+        funct.waitAndSend(driver, var.regV.city, entry_info[4])
 
-#Instructions for webdriver to read and input user data via the info on the .txt doc
-        # funct.waitAndSend(var.regV.fname, entry_info[0])
-        funct.waitUntil(var.regV.fname).send_keys(entry_info[0])
-        funct.waitUntil(var.regV.lname).send_keys(entry_info[1])
-        funct.waitUntil(var.regV.housenum).send_keys(entry_info[2])
-        funct.waitUntil(var.regV.city).send_keys(entry_info[4])
+# Find and select the state according to the info in the .txt doc
+        select_box = driver.find_element_by_name("state")
+        options = [x for x in select_box.find_elements_by_tag_name("option")]
+        for element in options:
+            if element.text == (entry_info[5]):
+                element.click()
+            else:
+                print(element.text + " does not match " + entry_info[5])
 
-#Find and select the state according to the info in the .txt doc
-        funct.waitAndClick(var.regV.state_dropdown)
-        driver.find_element_by_css_selector("#app-container > div > div.container__content > div > div > form > div:nth-child(1) > div.form-group.error > div > select > option:nth-child(38)").click()
+# # Declares the drop-down element as an instance of the Select class.
+#         Select drpState = new Select(driver.find_element_by_name("state"))
+#         drpState.selectByValue(entry_info[5])
 
-        zip.send_keys(entry_info[5])
-        phone.send_keys(entry_info[6])
-        ssn4.send_keys(entry_info[7])
-        #ss_check.click()   #not clicked for successful registration with SSN4
-        dob_month.send_keys(entry_info[8] + entry_info[9] + entry_info[10])
-        #dob_day.send_keys(entry_info[9])
-        #dob_year.send_keys(entry_info[10])
-        dob_check.click()
-        email.send_keys(testemail)
-        password.send_keys(entry_info[11])
-        passwordc.send_keys(entry_info[11])
-        tos_check.click()
-        submit_button.click()
-#implicit wait for #2nd screen to load elements
-        # driver.implicitly_wait(10)
-        # otp_header = driver.find_element_by_css_selector('#app-container > div > div.container__content > div > div > h3.confirm-otp-header')
+        funct.waitAndSend(driver, var.regV.zip, entry_info[6])
+        funct.waitAndSend(driver, var.regV.phone, entry_info[7])
+        funct.waitAndSend(driver, var.regV.ssn4, entry_info[8])
+        funct.waitAndSend(driver, var.regV.dob, (entry_info[9] + entry_info[10] + entry_info[11]))
+        funct.waitAndClick(driver, var.regV.dob_check)
+        funct.waitAndSend(driver, var.regV.email, testemail)
+        funct.waitAndSend(driver, var.regV.password, entry_info[12])
+        funct.waitAndSend(driver, var.regV.passwordc, entry_info[12])
+        funct.waitAndClick(driver, var.regV.tos_check)
+        funct.waitAndClick(driver, var.regV.submit_button)
+# 2nd screen. OTP selection screen
+        self.assertIn("Confirm Account", driver.page_source)
+        funct.waitAndClick(driver, var.otpV.text_button)
+# 3rd screen. OTP code entry screen
+        self.assertIn("Enter your code", driver.page_source)
+        funct.waitAndSend(driver, var.otpV.otp_input, "111111")
+        funct.waitAndClick(driver, var.otpV.otp_continue_button)
         time.sleep(10)
-#2nd screen. OTP selection screen
-        driver.find_element_by_css_selector('#app-container > div > div.container__content > div > div > div > button.nyl-btn-single.button-1').click()
-        driver.find_element_by_xpath("/html/body/div/div/div[2]/div/div/form/div/div[1]/div/input").send_keys("111111")
-        driver.find_element_by_css_selector('#app-container > div > div.container__content > div > div > form > div > div:nth-child(4) > button > span').click()
-        time.sleep(10)
-#3rd screen. Successful registration should redirect to Google.com. Checking that the search field on google.com is present on page.
+# 4th screen. Successful registration should redirect to Google.com.
+# Checking that the search field on google.com is present on page.
         if driver.find_elements_by_name("q") != []:
              print("registration successful and redirected to callback uri")
         else:
             driver.save_screenshot('test_screenshot_1.png')
             print("E---Redirect screen not reached")
         print("Test complete!")
-
+# The tearDown method will get called after every test method. This is a place to do all cleanup actions.
     def tearDown(self):
        # self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
+# Boiler plate code to run the test suite
 if __name__ == "__main__":
     unittest.main()
