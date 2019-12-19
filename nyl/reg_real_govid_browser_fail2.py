17570c4b2684b56797113d7d103212d4a50d303b
Marie Liao <marie.liao@rosedigital.co>
2019-12-19 14:25:47 -0500

    adding working file for Test case with fake US passport registration

---
+++ nyl/reg_real_govid_browser_fail2.py
@@ -0,0 +1,134 @@
+# [Documentation - Setup] This section lists all dependencies
+# that are imported for this test file to work
+from selenium import webdriver  #webdriver module provides all WebDriver implementations
+import warnings
+import unittest, time, re       #unittest is the testing framework, provides module for organizing test cases
+from selenium.webdriver.common.keys import Keys     #Keys class provide keys in the keyboard like RETURN, F1, ALT, etc.
+from selenium.webdriver.common.by import By         #By class provides method for finding the page elements by NAME, ID, XPATH, etc.
+import var                                          #Custom class for NYL
+import funct                                        #Custom class for NYL
+
+# [Documentation - Summary] Tests user workflow of failed
+# registration with OTP pass and fake US passport on Browser method
+# For use with Entry Info file version: nyl12122019.txt
+# For use with Image file versions: DLback.jpg, DLface.jpg, DLfront.jpg
+# USpassport.jpg, USface.jpg, Intlpassport.jpg, Intlpassportface.jpg
+# Change paths starting on Line 104 for reading images prior to running test
+
+# [Documentation - Variables] Test file specific var
+#url = "https://sso-dev.nylservices.net/?clientId=29d5np06tgg87unmhfoa3pkma7&redirectUri=https://google.com"
+url = "https://sso-qa.nylservices.net/?clientId=4a0p01j46oms3j18l90lbtma0o&callbackUri=https://google.com"
+#url = "https://sso-stage.nylservices.net/?clientId=6pdeoajlh4ttgktolu3jir8gp6&callbackUri=https://google.com"
+testemail = "marie.liao+ssotest@rosedigital.co"
+
+class NYlotto(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        # self.driver = webdriver.Remote(
+        #    command_executor='http://192.168.86.26:4444/wd/hub',
+        #    desired_capabilities= {
+        #        "browserName": "chrome",
+        #        "version": "",
+        #        "platform": "ANY",
+        #        "javascriptEnabled": True,
+        #        'chromeOptions': {
+        #            'useAutomationExtension': False,
+        #            'args': ['--disable-infobars']
+        #        }
+        #   })
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(12)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+# This is the test case method. The test case method should always start with the characters test.
+# The first line inside this method creates a local reference to the driver object created in setUp method.
+    def test_reg(self):
+        driver = self.driver
+        # opens local file with user data
+        notepadfile = open('/Users/nyl12122019.txt', 'r')
+        # variable for each line in the file
+        entry_info = notepadfile.read().splitlines()
+        # The driver.get method will navigate to a page given by the URL.
+        # WebDriver will wait until the page has fully loaded (that is, the “onload” event has fired)
+        # before returning control to your test or script.
+        driver.get(url)
+        # Assertion that the title has Single Sign On in the title.
+        self.assertIn("Single Sign On", driver.title)
+        # Instructions for webdriver to read and input user data via the info on the .txt doc.
+        funct.waitAndSend(driver, var.regV.fname, entry_info[0])
+        funct.waitAndSend(driver, var.regV.lname, entry_info[1])
+        funct.waitAndSend(driver, var.regV.housenum, entry_info[2])
+        funct.waitAndSend(driver, var.regV.street, entry_info[3])
+        funct.waitAndSend(driver, var.regV.city, entry_info[4])
+        # Find and select the state according to the info in the .txt doc
+        # Uses a for loop to iterate through the list of states until element
+        # matches the entry info in the text file. Then clicks the element found.
+        select_box = driver.find_element_by_name("state")
+        funct.waitAndClick(driver, var.regV.state_dropdown)
+        options = [x for x in select_box.find_elements_by_tag_name("option")]
+        for element in options:
+            if element.text in entry_info[5]:
+                element.click()
+                break
+        funct.waitAndSend(driver, var.regV.zip, entry_info[6])
+        funct.waitAndSend(driver, var.regV.phone, entry_info[7])
+        # Clicks the checkbox for not supplying SSN4 info. Will send user thru ID Verification flow.
+        funct.waitAndClick(driver, var.regV.ss_check)
+        funct.waitAndSend(driver, var.regV.dob, (entry_info[9] + entry_info[10] + entry_info[11]))
+        funct.waitAndClick(driver, var.regV.dob_check)
+        funct.waitAndSend(driver, var.regV.email, testemail)
+        funct.waitAndSend(driver, var.regV.password, entry_info[12])
+        funct.waitAndSend(driver, var.regV.passwordc, entry_info[12])
+        funct.waitAndClick(driver, var.regV.tos_check)
+        funct.waitAndClick(driver, var.regV.submit_button)
+# 2nd screen. OTP selection screen
+        funct.waitAndClick(driver, var.otpV.text_button)
+# 3rd screen. OTP code entry screen
+        funct.waitAndSend(driver, var.otpV.otp_input, "111111")
+        funct.waitAndClick(driver, var.otpV.otp_continue_button)
+        time.sleep(5)
+# 4th screen. Gov ID document capture selection screen
+# Choosing US Drivers License and Continue on Browser
+        funct.waitAndClick(driver, var.govIdV.gov_id_dropdown)
+        funct.waitAndClick(driver, var.govIdV.id_passport)
+        funct.waitAndClick(driver, var.govIdV.gov_id_dropdown)
+        funct.waitAndClick(driver, var.govIdV.browser_link)
+        time.sleep(5)
+# 5th screen. Initiate document capture process
+# uploading the different images for the gov id verification
+#        DLback.jpg, DLface.jpg, DLfront.jpg
+        funct.waitAndClick(driver, var.govIdV.dl_start_button)
+# 6th screen. Upload Front of Drivers's License & Save
+        funct.waitAndSend(driver, var.govIdV.dl_front_capture_button, "/Users/marieliao/Desktop/DLfront.jpg")
+# 7th screen. Quality check & Save
+        funct.waitAndClick(driver, var.govIdV.dl_front_save_button)
+# 8th screen. Upload Back of Driver's License
+        funct.waitAndSend(driver, var.govIdV.dl_back_capture_button, "/Users/marieliao/Desktop/DLback.jpg")
+# 9th screen. Quality check & Save
+        funct.waitAndClick(driver, var.govIdV.dl_back_save_button)
+# 10th screen. Upload Facial Snapshot
+        funct.waitAndSend(driver, var.govIdV.dl_facial_capture_button, "/Users/marieliao/Desktop/DLface.jpg")
+# 11th screen. Quality check & Save
+        funct.waitAndClick(driver, var.govIdV.dl_facial_save_button)
+# 12th screen. Submit all docs for id verification
+        funct.waitAndClick(driver, var.govIdV.dl_submit_button)
+# Last screen. Screen should show error message for identity verification.
+# Successful registration would redirect to Google.com. Checking that the search field on google.com is present on page.
+        if "Sorry, we were unable to verify your information." in driver.page_source:
+             print("ID Verification Failed message is expected and received!")
+        elif driver.find_elements_by_name("q") != []:
+            print("E----Reached valid registration screen and redirected to callback uri.")
+            driver.save_screenshot('test_screenshot_1.png')
+        else:
+            driver.save_screenshot('test_screenshot_2.png')
+            print("E---Neither Identity verification error message reached nor Registration success screen reached (or text is incorrect/needs to be updated)")
+        print("Test complete!")
+# The tearDown method will get called after every test method. This is a place to do all cleanup actions.
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+# Boiler plate code to run the test suite
+if __name__ == "__main__":
+    unittest.main()
