68ac19a1d10ab680c4fd2561db62b68acaf9f2a6
Patrick Foley <foley@roses-mbp-4.lan>
2020-02-10 15:17:14 -0500

    cleanup

--- nyl/confTest.py
+++ nyl/confTest.py
@@ -7,8 +7,7 @@ import unittest, time, re       #unittest is the testing framework, provides mod
 from selenium.webdriver.common.keys import Keys     #Keys class provide keys in the keyboard like RETURN, F1, ALT, etc.
 from selenium.webdriver.common.by import By         #By class provides method for finding the page elements by NAME, ID, XPATH, etc.
 from selenium.webdriver.support.ui import Select    #Select class provides ability to select items in dropdown
-import var, funct, util     
-import helper                        #Custom class for NYL
+import var, funct, util                          #Custom class for NYL
 
 #url = "https://sso-dev.nylservices.net/?clientId=29d5np06tgg87unmhfoa3pkma7&redirectUri=https://google.com"
 url = "https://sso-qa.nylservices.net/?clientId=4a0p01j46oms3j18l90lbtma0o&callbackUri=https://google.com"
