80d84e20637ee57eb4728b2f2db233fccf8ff6a6
Patrick Foley <foley@roses-mbp-4.lan>
2020-02-12 11:55:03 -0500

    added mobile and HTML test runner

--- nylMobile/funct.py
+++ nylMobile/funct.py
@@ -5,6 +5,7 @@ import unittest, time, re
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import util
+from selenium.webdriver import ActionChains
 
 # [Documentation - Summary] This file creates the functions for
 # use in the automation test suite of NYL SSO
@@ -12,12 +13,15 @@ import util
 # [Documentation - Function] Webdriver waits for a specified page element
 # to appear prior to the next interaction on the page
 def waitUntil(browser, elem):
-
+    a = ActionChains(browser)
     try:
         browser.find_element(elem[0], elem[1])
+        assert(browser.find_element(elem[0], elem[1]))
     except:
+        time.sleep(2)
         try:
-            browser.find_element(By.elem)
+            a.move_to_element(browser.find_element(elem[0], elem[1]))
+            assert(browser.find_element(elem[0], elem[1]))
         except:
             print("E--" + elem[1] + " elem not found")
 
@@ -33,15 +37,30 @@ def waitAndSend(browser, elem, keys):
     waitUntil(browser, elem)
     browser.find_element(elem[0], elem[1]).send_keys(keys)
 
+def actionSend(browser, keys):
+    a = ActionChains(browser)
+    a.send_keys(keys)
+    a.perform()
+
+
 # [Documentation - Function] Function that grabs UTC time and converts to human readable format
 def timeStamp():
     ts = time.gmtime()
     times = time.strftime("%Y-%m-%d_%H-%M-%S", ts)
     return times
 
-# [Documentation - Function] Function that calls the script to grab full page UTC timestamped screenshot
-def fullshot(self):
-    self.driver.set_window_position(0, 0)
-    self.driver.maximize_window()
-    timestamp = timeStamp() + '.png'
-    util.fullpage_screenshot(self.driver, timestamp)
+def checkText(browser, elem, stub):
+    waitUntil(browser, elem)
+    el = browser.find_element(elem[0], elem[1])
+    if el.text == stub:
+        assert el.text == stub
+    else:
+        print('E---Text Incorrect!\n\nExpected text: "' + stub + '"\n\n but text was: "' + el.text + '"')
+        assert el.text == stub
+
+# # [Documentation - Function] Function that calls the script to grab full page UTC timestamped screenshot
+# def fullshot(self):
+#     self.driver.set_window_position(0, 0)
+#     self.driver.maximize_window()
+#     timestamp = timeStamp() + '.png'
+#     util.fullpage_screenshot(self.driver, timestamp)
--- nylMobile/var.py
+++ nylMobile/var.py
@@ -6,108 +6,49 @@ from selenium.webdriver.common.by import By
 # NYL Single Sign On page objects for testing user flows
 
 # [Documentation - Variables] Objects on Registration page
-class regV:
-    fname = [By.NAME, "firstName"]
-    mname = [By.NAME, "middleName"]
-    lname = [By.NAME, "lastName"]
-    suffix_dropdown = [By.NAME, "suffix"]
-    housenum = [By.NAME, "streetNumber"]
-    street = [By.NAME, "street"]
-    add2 = [By.NAME, "addressLine2"]
-    city = [By.NAME, "city"]
-    state_dropdown = [By.NAME, "state"]
-    zip = [By.NAME, "zip"]
-    phone = [By.NAME, "phone"]
-    ssn4 = [By.NAME, "ssn4"]
-    ss_check = [By.NAME, "noSsn4"]
-    dob = [By.NAME, "birthdate"]
-    dob_check = [By.NAME, "isOver18"]
-    email = [By.ID, "sso-email"]
-    password = [By.NAME, "password"]
-    passwordc = [By.NAME, "confirmPassword"]
-    tos_check = [By.NAME, "acceptedTermsAndConditions"]
-    cnw_check = [By.NAME, "collectnwin"]
-    nylnews_check = [By.NAME, "newsletter"]
-    submit_button = [By.CLASS_NAME, "nyl-btn"]
+class NYLdashboard:
 
-# [Documentation - Variables] Objects on OTP pages
-class otpV:
-# otp method selection page
-    text_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > div > button.nyl-btn-single.button-1 > span"]
-    call_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > div > button.nyl-btn-single.button-2 > span"]
-# otp code entry page
-    otp_input = [By.NAME, "otp"]
-    otp_continue_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > form > div > div:nth-child(4) > button > span"]
-    retry_call_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > form > div > div:nth-child(5] > p > button:nth-child(1]"]
-    retry_text_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > form > div > div:nth-child(5] > p > button:nth-child(2]"]
+    guest_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/guestButton"]']
+    login_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/loginButton"]']
+    register_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/loginRegister"]']
+    guestCopy = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/guestCopy"]']
+    loginCopy = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/loginCopy"]']
+    ForgotPwCopy = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/loginForgotPass"]']
+    username_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/loginEmail"]']
+    password_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/loginPassword"]']
+    back_b = [By.XPATH, '//*[@content-desc="Navigate up"]']
 
 
-# Drivers license and browser capture method
-# Document capture page
-    dl_start_button = [By.ID, "dcui-start-button"]
-# Driver's license front capture page
-    dl_front_capture_button = [By.ID, "capture-input"]
-# Driver's license front quality check page
-    dl_front_discard_button = [By.ID, "discard-capture"]
-    dl_front_save_button = [By.ID, "save-capture"]
-# Driver's license back capture page
-    dl_back_capture_button = [By.ID, "capture-input"]
-# Driver's license back quality check page
-    dl_back_discard_button = [By.ID, "discard-capture"]
-    dl_back_save_button = [By.ID, "save-capture"]
-# Facial snapshot capture page
-    dl_facial_capture_button = [By.ID, "capture-input"]
-# Facial snapshot quality check page
-    dl_facial_discard_button = [By.CSS_SELECTOR, "#discard-capture"]
-    dl_facial_save_button = [By.CSS_SELECTOR, "#save-capture"]
-# Document submission page
-    dl_submit_button = [By.ID, "verify-all"]
-
-# Passport and browser capture method
-# Document capture page
-    passport_start_button = [By.ID, "dcui-start-button"]
-# Passport front capture page
-    passport_capture_button = [By.ID, "capture-input"]
-# Passport front quality check page
-    passport_discard_button = [By.ID, "discard-capture"]
-    passport_save_button = [By.ID, "save-capture"]
-# Facial snapshot capture page
-    passport_facial_capture_button = [By.ID, "capture-input"]
-# Facial snapshot quality check page
-    passport_facial_discard_button = [By.CSS_SELECTOR, "#discard-capture"]
-    passport_facial_save_button = [By.CSS_SELECTOR, "#save-capture"]
-# Document submission page
-    passport_submit_button = [By.ID, "verify-all"]
-
-# May be completed at a future date with Appium inspection session
-# Drivers license and mobile capture method
-# Passport and mobile capture method
-
-# [Documentation - Variables] Objects on Login page
-class loginV:
-    email = [By.ID, "sso-email"]
-    password = [By.NAME, "password"]
-    login_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > form > div > div.button-wrap > button > span"]
-    forgot_password_link = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > form > div > div.button-wrap > p > a"]
-
-# [Documentation - Variables] Objects on Reset Password page
-class resetPswV:
-    email = [By.NAME, "email"]
-    reset_submit_button = [By.CSS_SELECTOR, "#app-container > div > div.container__content > div > div > form > div > div.button-wrap > button > span"]
-
-# [Documentation - Variables] Objects on Update Profile page
-class updateProfV:
-    fname = [By.NAME, "firstName"]
-    mname = [By.NAME, "middleName"]
-    lname = [By.NAME, "lastName"]
-    suffix = [By.NAME, "suffix"]
-    housenum = [By.NAME, "streetNumber"]
-    street = [By.NAME, "street"]
-    add2 = [By.NAME, "addressLine2"]
-    city = [By.NAME, "city"]
-    state = [By.NAME, "state"]
-    zip = [By.NAME, "zip"]
-    phone = [By.NAME, "phone"]
-    dob = [By.NAME, "birthdate"]
-    email = [By.ID, "sso-email"]
-    update_submit_button = [By.CLASS_NAME, "nyl-btn"]
+    guestStub = 'Check winning numbers, prizes, and draw dates\nwithout a New York Lottery account'
+    loginStub = 'Log in for the full Lottery experience, including access to Players Club and Second Chance Sweepstakes'
+class NYLregistration:
+    #main-fields
+    email_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerEmail"]']
+    password_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerPassword"]']
+    password_copy = [By.XPATH, 'air.com.eprize.nylottery.app.NYLotteryApp:id/registerPasswordError']
+    confirmPW_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerConfirmPass"]']
+    firstName_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerFirstName"]']
+    lastName_f = [By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.RelativeLayout/android.widget.EditText[5]']
+    phoneDropDown = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerPhoneType"]']
+    phoneDropDown_HOME = [By.XPATH, '//*[@text="HOME"]']
+    phoneDropDown_MOBILE = [By.XPATH, '//*[@text="MOBILE"]']
+    phoneDropDown_WORK = [By.XPATH, '//*[@text="WORK"]']
+    phone_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerPhone"]']
+    age_copy = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/ageQuestion"]']
+    yes_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerYes"]']
+    nope_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerNo"]']
+    zip_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerZip"]']
+    newsletter_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/newsletter"]']
+    reg_b = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerButton"]']
+    #error fields
+    email_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerEmailError"]']
+    password_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerPasswordError"]']
+    confirmPW_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerConfirmPassError"]']
+    firstName_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerFirstNameError"]']
+    lastName_f = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerLastNameError"]']
+    phone_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerPhoneError"]']
+    age_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerAgeError"]']
+    zip_e = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/registerZipError"]']
+    #success screen variables
+    pip1 = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/intro_indicator_0"]']    
+    pip6 = [By.XPATH, '//*[@resource-id="air.com.eprize.nylottery.app.NYLotteryApp:id/intro_indicator_5"]']    
