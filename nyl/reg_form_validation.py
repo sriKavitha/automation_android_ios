ce106a17a11a7ac2675597a3ef9b2337cedb71b1
Marie Liao <marie.liao@rosedigital.co>
2020-03-03 15:29:41 -0500

    removing hard fail test case from reg_form_validation.py

--- nyl/reg_form_validation.py
+++ nyl/reg_form_validation.py
@@ -323,45 +323,6 @@ class NYlotto(confTest.NYlottoBASE):
             funct.fullshot(driver)
             raise Exception('Error warning(s) missing.')
 
-# Checks for red text color change in Checkbox errors
-    def test_17regChkbxErrors(self):
-        driver = self.driver
-        driver.get(url)
-        # putting in acceptable but invalid data
-        funct.waitAndSend(driver, var.regV.fname, "Fake")
-        funct.waitAndSend(driver, var.regV.lname, "Test")
-        funct.waitAndSend(driver, var.regV.housenum, "12345")
-        funct.waitAndSend(driver, var.regV.street, "First Street")
-        funct.waitAndSend(driver, var.regV.city, "Anytown")
-        funct.waitAndClick(driver, var.regV.state_dropdown)
-        driver.find_element_by_css_selector("#app-container > div > div.container__content > div > div > form > div:nth-child(1) > div:nth-child(10) > div > select > option:nth-child(2)").click()
-        funct.waitAndSend(driver, var.regV.zip, "11223")
-        funct.waitAndSend(driver, var.regV.phone, "5559876543")
-        funct.waitAndSend(driver, var.regV.ssn4, "1234")
-        funct.waitAndSend(driver, var.regV.dob, "01/01/1990")
-        funct.waitAndClick(driver, var.regV.dob_check)
-        funct.waitAndSend(driver, var.regV.email, "qa@testemail.co")
-        funct.waitAndSend(driver, var.regV.password, "Test1234")
-        funct.waitAndSend(driver, var.regV.confirmPsw, "Test1234")
-        funct.waitAndClick(driver, var.regV.tos_check)
-        funct.waitAndClick(driver, var.regV.submit_button)
-
-        # checking that we get to the "can not verify your identity" screen
-        try:
-            driver.find_elements_by_class_name("migration-failed-body")
-        except:
-            print("Can not find 'Identity verification failed' screen")
-        if "Sorry, we cannot verify your identity." in driver.page_source:
-             print("PASS - Identity verification failed screen reached.")
-        elif driver.find_elements_by_name("q") != []:
-            print("FAIL - Reached successful registration and redirected to callback uri (Google.com)")
-            funct.fullshot(driver)
-            raise Exception('Registration succeeded where it was supposed to fail.')
-        else:
-            print("FAIL - Neither Identity verification failed screen nor Registration successful screen reached.")
-            funct.fullshot(driver)
-            raise Exception('Registration redirected incorrectly.')
-
 # Boiler plate code to run the test suite
 if __name__ == "__main__":
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='<html_report_dir>'))
