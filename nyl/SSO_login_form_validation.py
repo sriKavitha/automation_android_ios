22f9ee5be41c6eb6248c0999a35c3d3281960045
Marie Liao <marie.liao@rosedigital.co>
2021-09-03 10:11:47 -0400

    consolidated login validation tests and improved print statement readability

--- nyl/SSO_login_form_validation.py
+++ nyl/SSO_login_form_validation.py
@@ -10,8 +10,10 @@ import var, funct, util, confTest, HtmlTestRunner   #Custom class for NYL
 
 class NYlotto(confTest.NYlottoBASE):
 
-# Checks main error appears when empty form is submitted
-    def test01_loginError(self):
+    def test_01_loginError(self):
+        testenv = self.env
+        print("\nTESTING " + testenv + " ENVIRONMENT")
+        print("\nChecks main error appears when empty form is submitted and text copy is correct")
         driver = self.driver
         # url is pulled from confTest
         driver.get(self.login_url)
@@ -23,25 +25,18 @@ class NYlotto(confTest.NYlottoBASE):
             print('FAIL - ' + var.loginV.login_button_error[2] + ' is missing')
             funct.fullshot(driver)
             raise Exception('Error warning element not found')
-
-# Checks main error copy
-    def test02_loginErrorCopy(self):
-        driver = self.driver
-        # url is pulled from confTest
-        driver.get(self.login_url)
-        # triggering error
-        funct.waitAndClick(driver, var.loginV.login_button)
         warning = driver.find_element(var.loginV.login_button_error[0], var.loginV.login_button_error[1])
         if funct.checkErrorText(driver, var.loginV.login_button_error, var.loginV.loginErrorStub) == True:
-            print('PASS - Warning copy text is correct')
-            print('Warning text displayed is " ' + warning.get_attribute("innerText") + ' "')
+            print('PASS - Warning copy is correct')
         elif funct.checkErrorText(driver, var.loginV.login_button_error, var.loginV.loginErrorStub) == False:
             print('FAIL - Warning should say "' + var.loginV.loginErrorStub + '" , but says "' + warning.get_attribute("innerText") + '"')
             funct.fullshot(driver)
             raise Exception('Error copy is incorrect')
+        print('----------')
+        funct.closeWindow(driver, 'New York Lottery - Single Sign On')
 
-# Checks mandatory field errors are found
-    def test03_loginRequiredErrors(self):
+    def test_02_loginRequiredErrors(self):
+        print("\nChecks mandatory field errors are found and text copy is correct")
         driver = self.driver
         # url is pulled from confTest
         driver.get(self.login_url)
@@ -56,40 +51,30 @@ class NYlotto(confTest.NYlottoBASE):
             if funct.checkError(driver, warning) == False:
                 warningList.append(warning[2])
         if len(warningList) <= 0:
-            print('PASS - Error warnings found and present')
+            print('PASS - Error warnings present')
         elif len(warningList) > 0:
             print('FAIL - ')
             print(warningList)
             print(' Error warning(s) missing')
             funct.fullshot(driver)
             raise Exception('Error warning element not found')
-
-# Checks mandatory field error copy
-    def test04_loginRequiredErrorsCopy(self):
-        driver = self.driver
-        # url is pulled from confTest
-        driver.get(self.login_url)
-        # triggering error
-        funct.waitAndClick(driver, var.loginV.email)
-        funct.waitAndClick(driver, var.loginV.password)
-        funct.waitAndClick(driver, var.loginV.login_button)
-        warningList = []
-        # These are the CSS selectors for the 2 red text error elements
         warningsExpected = [var.loginV.email_error, var.loginV.password_error]
         for warning in warningsExpected:
             if funct.checkErrorText(driver, warning, var.loginV.requiredErrorStub) == False:
                 warningList.append(warning[2])
         if len(warningList) <= 0:
-            print('PASS - Error warnings found and copy is correct')
+            print('PASS - Error warnings copy is correct')
         elif len(warningList) > 0:
             print('FAIL - ')
             print(warningList)
             print('Error warning(s) copy is incorrect')
             funct.fullshot(driver)
             raise Exception('Error warning(s) copy is incorrect')
+        print('----------')
+        funct.closeWindow(driver, 'New York Lottery - Single Sign On')
 
-# Checks missing email login attempt error
-    def test05_loginIncompleteEmailError(self):
+    def test_03_loginIncompleteEmailError(self):
+        print("\nChecks missing email login attempt error and text copy is correct")
         driver = self.driver
         # url is pulled from confTest
         driver.get(self.login_url)
@@ -106,14 +91,15 @@ class NYlotto(confTest.NYlottoBASE):
         warning = driver.find_element(var.loginV.login_button_error[0], var.loginV.login_button_error[1])
         if funct.checkErrorText(driver, var.loginV.login_button_error, var.loginV.loginErrorStub) == True:
             print('PASS - Warning copy text is correct')
-            print('Warning text displayed is " ' + warning.get_attribute("innerText") + '"')
         elif funct.checkErrorText(driver, var.loginV.login_button_error, var.loginV.loginErrorStub) == False:
             print('FAIL - Warning should say " ' + var.loginV.loginErrorStub + ' , but says "' + warning.get_attribute("innerText") + '"')
             funct.fullshot(driver)
             raise Exception('Error copy is incorrect')
+        print('----------')
+        funct.closeWindow(driver, 'New York Lottery - Single Sign On')
 
-# Checks missing password login attempt error
-    def test06_loginIncompletePswError(self):
+    def test_04_loginIncompletePswError(self):
+        print("\nChecks missing password login attempt error and text copy is correct")
         testemail = self.testemail
         driver = self.driver
         # url is pulled from confTest
@@ -130,15 +116,16 @@ class NYlotto(confTest.NYlottoBASE):
 
         warning = driver.find_element(var.loginV.login_button_error[0], var.loginV.login_button_error[1])
         if funct.checkErrorText(driver, var.loginV.login_button_error, var.loginV.loginErrorStub) == True:
-            print('PASS - Warning copy text is correct')
-            print('Warning text displayed is "' + warning.get_attribute("innerText") + '"')
+            print('PASS - Warning copy is correct')
         elif funct.checkErrorText(driver, var.loginV.login_button_error, var.loginV.loginErrorStub) == False:
             print('FAIL - Warning should say " ' + var.loginV.loginErrorStub + ' , but says "' + warning.get_attribute("innerText") + '"')
             funct.fullshot(driver)
             raise Exception('Error copy is incorrect')
+        print('----------')
+        funct.closeWindow(driver, 'New York Lottery - Single Sign On')
 
-# Checks that invalid email input displays error
-    def test07_loginInvalidEmailError(self):
+    def test_05_loginInvalidEmailError(self):
+        print("\nChecks that invalid email input displays error and text copy is correct")
         driver = self.driver
         # url is pulled from confTest
         driver.get(self.login_url)
@@ -146,33 +133,24 @@ class NYlotto(confTest.NYlottoBASE):
         funct.waitAndSend(driver, var.loginV.email, "bademail$$$%%%rosedigital")
         funct.waitAndClick(driver, var.loginV.login_button)
         if funct.checkError(driver, var.loginV.email_error) == True:
-            print('PASS - "' + var.loginV.email_error[2] + ' is present')
+            print('PASS - ' + var.loginV.email_error[2] + ' is present')
         elif funct.checkError(driver, var.loginV.email_error) == False:
             print('FAIL - "' + var.loginV.email_error[2] + ' is missing')
             funct.fullshot(driver)
             raise Exception('Error warning element not found')
-
-# Checks that invalid email input displays correct error message
-    def test08_loginInvalidEmailErrorCopy(self):
-        driver = self.driver
-        # url is pulled from confTest
-        driver.get(self.login_url)
-        # triggering error
-        funct.waitAndSend(driver, var.loginV.email, "bademail$$$%%%rosedigital")
-        funct.waitAndClick(driver, var.loginV.login_button)
-
         warning = driver.find_element(var.loginV.email_error[0], var.loginV.email_error[1])
         if funct.checkErrorText(driver, var.loginV.email_error, var.loginV.emailErrorStub) == True:
-            print('PASS - Error warnings found and copy is correct')
-            print('Warning text displayed is "' + warning.get_attribute("innerText") + '"')
+            print('PASS - Error warnings copy is correct')
         elif funct.checkErrorText(driver, var.loginV.email_error, var.loginV.emailErrorStub) == False:
             print('FAIL - Warning should say "' + var.loginV.emailErrorStub + ' , but says "' + warning.get_attribute("innerText") + '"')
             funct.fullshot(driver)
             raise Exception('Error warning(s) copy is incorrect')
+        print('----------')
+        funct.closeWindow(driver, 'New York Lottery - Single Sign On')
 
 
 # Boiler plate code to run the test suite
 if __name__ == "__main__":
     # First runner will enable html logs on your current directory, second runner will keep local console logs
-    unittest.main(warnings='ignore', testRunner=HtmlTestRunner.HTMLTestRunner(output='<html_report_dir>'))
-    # unittest.main(warnings='ignore')
+    # unittest.main(warnings='ignore', testRunner=HtmlTestRunner.HTMLTestRunner(output='<html_report_dir>'))
+    unittest.main(warnings='ignore')
