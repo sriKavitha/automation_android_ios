5ab012b632c75ff75e0d889103e6b965b93b8c0f
Marie Liao <marie.liao@rosedigital.co>
2022-01-18 15:10:28 -0500

    added bad otp test

---
+++ nyl/SSO_reg_otp_bad_fail.py
@@ -0,0 +1,127 @@
+# [Documentation - Setup] This section lists all dependencies
+# that are imported for this test file to work
+from selenium import webdriver  #webdriver module provides all WebDriver implementations
+import warnings
+import unittest, time, re       #unittest is the testing framework, provides module for organizing test cases
+from selenium.webdriver.common.keys import Keys     #Keys class provide keys in the keyboard like RETURN, F1, ALT, etc.
+from selenium.webdriver.common.by import By         #By class provides method for finding the page elements by NAME, ID, XPATH, etc.
+from selenium.webdriver.support.ui import Select    #Select class provides ability to select items in dropdown
+import var, funct, util, confTest    #Custom class for NYL
+import HtmlTestRunner
+
+# [Documentation - Summary] Tests user workflow of failed
+# registration with submitting incorrect OTP
+
+# The test case ex is inherited from unittest.TestCase.
+# Inheriting from TestCase class is the way to tell unittest module that this is a test case.
+class NYlotto(confTest.NYlottoBASE):
+
+# This is the test case method. The test case method should always start with the characters test.
+# The first line inside this method creates a local reference to the driver object created in setUp method.
+    def test_regBadOTPFail(self):
+        testemail = self.testemail
+        testenv = self.env
+        print("TESTING " + testenv + " ENVIRONMENT")
+
+        print('\n----------\n' + 'Test setup')
+        # Check for existing test user and wipe it from userpool prior to test execution
+        try:
+            funct.purgeSSOemail(self, testemail)
+            if self.env != 'dev':
+                try:
+                    funct.purgeSSOphone(self, var.credsSSOWEB.phone)
+                except:
+                    pass
+        except:
+            if self.env != 'dev':
+                try:
+                    funct.purgeSSOphone(self, var.credsSSOWEB.phone)
+                except:
+                    pass
+        print('----------')
+
+        driver = self.driver
+        # The driver.get method will navigate to a page given by the URL.
+        # WebDriver will wait until the page has fully loaded (that is, the “onload” event has fired)
+        # before returning control to your test or script.
+        # url is pulled from confTest
+        driver.get(self.reg_url)
+        # Assertion that the title has Single Sign On in the title.
+        self.assertIn("Single Sign On", driver.title)
+
+        # Instructions for webdriver to read and input user data via the info on the .txt doc.
+        # Credentials are localized to one instance via the var file
+        funct.waitAndSend(driver, var.regV.fname, var.credsSSOWEB.fname)
+        funct.waitAndSend(driver, var.regV.lname, var.credsSSOWEB.lname)
+        funct.waitAndSend(driver, var.regV.housenum, var.credsSSOWEB.housenum)
+        funct.waitAndSend(driver, var.regV.street, var.credsSSOWEB.street)
+        funct.waitAndSend(driver, var.regV.city, var.credsSSOWEB.city)
+        # Find and select the state according to the info in the .txt doc
+        # Uses a for loop to iterate through the list of states until element
+        # matches the entry info in the text file. Then clicks the element found.
+        select_box = driver.find_element_by_name("state")
+        funct.waitAndClick(driver, var.regV.state_dropdown)
+        options = [x for x in select_box.find_elements_by_tag_name("option")]
+        for element in options:
+            if element.text in var.credsSSOWEB.state:
+                element.click()
+                break
+        funct.waitAndSend(driver, var.regV.zip, var.credsSSOWEB.zip)
+        funct.waitAndSend(driver, var.regV.phone, var.credsSSOWEB.phone)
+        funct.waitAndSend(driver, var.regV.ssn4, var.credsSSOWEB.ssn4)
+        funct.waitAndSend(driver, var.regV.dob, (var.credsSSOWEB.dob_month + var.credsSSOWEB.dob_date + var.credsSSOWEB.dob_year))
+        funct.waitAndClick(driver, var.regV.dob_check)
+        funct.waitAndSend(driver, var.regV.email, testemail)
+        funct.waitAndSend(driver, var.regV.password, var.credsSSOWEB.password)
+        funct.waitAndSend(driver, var.regV.confirmPsw, var.credsSSOWEB.password)
+        funct.waitAndClick(driver, var.regV.tos_check)
+        funct.waitAndClick(driver, var.regV.submit_button)
+        # 2nd screen. OTP selection screen
+        funct.waitAndClick(driver, var.otpV.text_button)
+        # 3rd screen. OTP code entry screen
+        funct.waitAndSend(driver, var.otpV.otp_input, "987654")     # incorrect code
+        time.sleep(15)
+        funct.waitAndClick(driver, var.otpV.otp_continue_button)
+        # checking redirects to "Sorry, identity cannot be verified" screen.
+        time.sleep(5)
+        if driver.find_elements_by_class_name("migration-failed-body") != []:
+            print(f"PASS - Correctly redirected to 'Identity verification failed' screen.")
+        elif driver.find_elements_by_name("q") != []:
+            print("FAIL - Identity verification failed screen NOT reached. Registration succeeded where it was supposed to fail.")
+            funct.fullshot(driver)
+            try:
+                funct.purgeSSOemail(self, testemail)
+            except:
+                print('no test user found')
+            raise Exception('Identity verification failed screen NOT reached.')
+        elif "Confirm your details" in driver.find_element_by_tag_name('body').text:
+            print("FAIL - Identity verification failed screen NOT reached. Redirected to Confirm Details screen")
+            funct.fullshot(driver)
+            try:
+                funct.purgeSSOemail(self, testemail)
+            except:
+                print('no test user found')
+            raise Exception("Identity verification failed screen NOT reached.")
+        else:
+            print("Unexpected screen reached when attempting to submit bad otp. See screenshot.")
+            funct.fullshot(driver)
+            try:
+                funct.purgeSSOemail(self, testemail)
+            except:
+                print('no test user found')
+            raise Exception("Identity verification failed screen NOT reached.")
+
+        # Deleting test data
+        print('\n----------\n' + 'Test complete!\n\nTest clean up commencing')
+        try:
+            funct.purgeSSOemail(self, testemail)
+        except:
+            pass
+        print('----------')
+
+# use "report" variable in conftest.py to change report style on runner
+if __name__ == "__main__":
+    if  confTest.NYlottoBASE.report == "terminal":
+        unittest.main(warnings='ignore')
+    elif confTest.NYlottoBASE.report == "html":
+        unittest.main(warnings='ignore', testRunner=HtmlTestRunner.HTMLTestRunner(output='<html_report_dir>'))
