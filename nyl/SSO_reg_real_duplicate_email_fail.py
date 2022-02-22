59620fa2f3ace5147f5135754fddbf5d109eb135
Marie Liao <marie.liao@rosedigital.co>
2022-02-22 14:32:43 -0500

    refactor dupe email test to use admin dash to edit user phone

--- nyl/SSO_reg_real_duplicate_email_fail.py
+++ nyl/SSO_reg_real_duplicate_email_fail.py
@@ -18,14 +18,106 @@ class NYlotto(confTest.NYlottoBASE):
         # Jira test ticket - https://rosedigital.atlassian.net/browse/NYL-1923
         testenv = self.env
         print("TESTING " + testenv + " ENVIRONMENT")
-        # Jira test ticket - https://rosedigital.atlassian.net/browse/NYL-1923
         print("\nChecks for failed registration with duplicate email in userpool")
         testemail = self.testemail
+
+        tempphone = '3472929732'
+        formatted_tempphone = '(' + tempphone[:3] + ') ' + tempphone[3:6] + '-' + tempphone[6:]
+
         driver = self.driver
         print('\n----------\n' + 'Test setup')
         # creates a verified user with valid SSN4
         funct.createVerifiedUser(self, testemail)
+        # edits the created user to a different phone number. so the system will only check the dupe email response
+        self.admin_url = 'https://admin-' + self.env + '.nylservices.net/'
+        driver.get(self.admin_url)
+        try:  # try to login
+            funct.waitAndFind(driver, var.adminLoginVar.signin_button)
+            funct.waitAndSend(driver, var.adminLoginVar.email, var.CREDSadmin.superadmin_username)
+            funct.waitAndSend(driver, var.adminLoginVar.password, var.CREDSadmin.superadmin_psw)
+            funct.waitAndClick(driver, var.adminLoginVar.signin_button)
+        except Exception:  # if session persists from before, extend session and continue
+            time.sleep(2)
+            try:
+                funct.waitAndFind(driver, var.adminDashVar.extend_button)
+                funct.waitAndClick(driver, var.adminDashVar.extend_button)
+            except:
+                pass  # Search for test user via Email
+        time.sleep(2)
+        funct.waitAndClick(driver, var.adminDashVar.search_input)
+        funct.waitAndSend(driver, var.adminDashVar.search_input, testemail)
+        try:
+            funct.waitAndClick(driver, var.adminDashVar.search_button)
+            time.sleep(1)
+            funct.waitAndClick(driver, var.adminDashVar.search_button)
+            time.sleep(3)
+        except:
+            time.sleep(2)
+            try:
+                funct.waitAndClick(driver, var.adminDashVar.search_button)
+            except:
+                funct.waitAndClick(driver, var.adminDashVar.search_button)
+
+        time.sleep(3)
+        # Checks the returned user is the correct user
+        rows = driver.find_elements_by_xpath('//tr[@class="ant-table-row ant-table-row-level-0"]')
+        if len(rows) == 1:
+            if driver.find_element_by_xpath(
+                '//td[@class="ant-table-cell"][4]').text == testemail:  # check that first user returned has the same email address
+                funct.waitAndClick(driver, var.adminDashVar.view_button)    # Clicks the View/Edit link
+                funct.waitAndClick(driver, var.adminEditVar.edit_button)    # Clicks the Edit info button
+                # funct.clearTextField(driver, var.adminDashVar.phone_input)
+                driver.find_element_by_xpath('//*[@id="phone"]').send_keys("" + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE + Keys.BACKSPACE)
+                funct.waitAndSend(driver, var.adminEditVar.phone_input, formatted_tempphone)
+                funct.waitAndClick(driver, var.adminEditVar.save_button)
+                # attempt to click the modal "OK" buttons to proceed to next step
+                # different locator for same button depending on new session or extended session
+                try:
+                    funct.waitAndClick(driver, var.adminDashVar.modal_ok_button)
+                except:
+                    try:
+                        funct.waitAndClick(driver, var.adminDashVar.ext2_modal_ok_button)
+                    except:
+                        try:
+                            funct.waitAndClick(driver, var.adminDashVar.ext1_modal_ok_button)
+                        except:
+                            try:
+                                funct.waitAndClick(driver, var.adminDashVar.extend_button)
+                            except:
+                                pass
+
+                funct.waitAndSend(driver, var.adminDashVar.comment_phrase_textarea, "edit user")
+
+                try:
+                    funct.waitAndClick(driver, var.adminDashVar.modal_ok_button)
+                except:
+                    try:
+                        funct.waitAndClick(driver, var.adminDashVar.ext2_modal_ok_button)
+                    except:
+                        try:
+                            funct.waitAndClick(driver, var.adminDashVar.ext1_modal_ok_button)
+                        except:
+                            try:
+                                funct.waitAndClick(driver, var.adminDashVar.extend_button)
+                            except:
+                                pass
+
+                try:
+                    funct.waitAndClick(driver, var.adminDashVar.modal_ok_button)
+                except:
+                    try:
+                        funct.waitAndClick(driver, var.adminDashVar.ext2_modal_ok_button)
+                    except:
+                        try:
+                            funct.waitAndClick(driver, var.adminDashVar.ext1_modal_ok_button)
+                        except:
+                            try:
+                                funct.waitAndClick(driver, var.adminDashVar.extend_button)
+                            except:
+                                pass
         print('----------')
+
+
         # Switch to blank registration page
         driver.get(self.reg_url)
         # Assertion that the title has Single Sign On in the title.
@@ -49,7 +141,7 @@ class NYlotto(confTest.NYlottoBASE):
                 element.click()
                 break
         funct.waitAndSend(driver, var.regV.zip, var.credsSSOWEB.zip)
-        funct.waitAndSend(driver, var.regV.phone, "5552221234")
+        funct.waitAndSend(driver, var.regV.phone, var.credsSSOWEB.phone)
         funct.waitAndSend(driver, var.regV.ssn4, var.credsSSOWEB.ssn4)
         funct.waitAndSend(driver, var.regV.dob,
                           (var.credsSSOWEB.dob_month + var.credsSSOWEB.dob_date + var.credsSSOWEB.dob_year))
@@ -90,6 +182,7 @@ class NYlotto(confTest.NYlottoBASE):
             funct.purgeSSOemail(self, testemail)
         except:
             pass
+        funct.closeWindow(driver, 'New York Lottery - Admin Dashboard')
         print('----------')
 
 # use "report" variable in conftest.py to change report style on runner
