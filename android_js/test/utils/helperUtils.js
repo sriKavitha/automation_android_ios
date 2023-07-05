f79f16309e434820316a13792db15027b3945d4e
kavitha sriram <kavitha9@hotmail.com>
2023-07-05 11:40:53 -0400

    Manual testcase MRMNYL-196 has been automated

--- android_js/test/utils/helperUtils.js
+++ android_js/test/utils/helperUtils.js
@@ -37,5 +37,44 @@ class Utils {
       await GuestPage.getStartedBtn.waitForDisplayed();
       await GuestPage.getStartedBtn.click();
   }
+
+  // Swipe forward
+  async swipeForward()
+  {
+      await driver.pause(2000)
+      await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollForward()');
+  }
+
+  // Swipe backward
+  async swipeBackward()
+  {
+      await driver.pause(2000)
+      await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollBackward()');
+  }
+
+    // swipe till game
+    async  swipeTillGame(page, thisgame_balls) {
+      var balls = await page.gameNumberOfBalls.length;
+      console.log("before::::",balls)
+      while (true) 
+      {
+        balls = await page.gameNumberOfBalls.length;
+        console.log("begin_1:",balls)
+        
+        if (balls === thisgame_balls) 
+        {
+            // tap on LOTTO game
+            console.log("before clicking on actual game:",balls)
+            await driver.pause(5000)
+            await page.NumberGame.click();
+            break
+        } else 
+        {
+            balls = await page.gameNumberOfBalls.length;
+            console.log("before swipe:",balls)
+            await this.swipeForward();
+        }
+      }
+    }
 }
 module.exports = new Utils();
